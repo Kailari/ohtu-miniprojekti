c821576f5bdfcde396f06ae16fda80b6f1108039
Robert <robert.pakkanen@helsinki.fi>
2019-11-26 00:43:30 +0200

    refactoring

--- src/main/java/winkkari/main/Main.java
+++ src/main/java/winkkari/main/Main.java
@@ -10,9 +10,9 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        if (System.getenv("PORT") != null) {
+        /*if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
-        }
+        }*/
         LOG.trace("Starting...");
         final var dbDAO = new DatabaseDAO();
         final var winkkari = new Winkkari(dbDAO);
